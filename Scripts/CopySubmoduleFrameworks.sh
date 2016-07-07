f976411a32040f40445c852ad74d7124a87f8718
Anton Efimenko <reloni@ya.ru>
2016-07-07 12:01:18 +0300

    Add script that copies working frameworks from submodules
    This should allow travis to correctly build and test project

---
+++ Scripts/CopySubmoduleFrameworks.sh
@@ -0,0 +1,2 @@
+# Copy RxHttpClient.framework, because it's used as submodule and may be changed while development
+cp -R ./XCodeBuild/Products/Debug-iphonesimulator/RxHttpClient.framework ./Dependencies/iOS
