fb51847eb266bb7a24fce24e862452d9644f0086
Anton Efimenko <reloni@ya.ru>
2016-07-08 13:34:17 +0300

    Fix CopySubmoduleFrameworks.sh

--- Scripts/CopySubmoduleFrameworks.sh
+++ Scripts/CopySubmoduleFrameworks.sh
@@ -1,5 +1,3 @@
 # Copy RxHttpClient.framework, because it's used as submodule and may be changed while development
-if [ -d ./XCodeBuild ]
-then
 cp -R ./XCodeBuild/Products/Debug-iphonesimulator/RxHttpClient.framework ./Dependencies/iOS
-fi
+exit 0
