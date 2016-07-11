c5b1a974358f398672d2e15a2018497039ae3211
Anton Efimenko <reloni@ya.ru>
2016-07-11 13:48:57 +0400

    Submodules deleted (#5)
    * Setup 1
    
    * Delete submodules
    
    * Add workspace and rebuild dependencies
    
    * Rebuild dependent framework to allow testing
    
    * Disable embedding carthage frameworks
    
    * Rebuild dependencies

--- Scripts/CarthageUpdate.sh
+++ Scripts/CarthageUpdate.sh
@@ -1,8 +1,8 @@
 #rm -rf ~/Library/Caches/org.carthage.CarthageKit
-carthage update --no-build --use-submodules
-carthage build RxSwift --platform iOS
-carthage build RxHttpClient --platform iOS
-carthage build AliSoftware/OHHTTPStubs --platform iOS
+carthage update --platform iOS
+#carthage build RxSwift --platform iOS
+#carthage build RxHttpClient --platform iOS
+#carthage build AliSoftware/OHHTTPStubs --platform iOS
 mkdir -p ./Dependencies/iOS
 ./Scripts/CopyFrameworks.sh
 #cp -R ./Carthage/Build/iOS/*.framework ./Dependencies/iOS
--- Scripts/CopySubmoduleFrameworks.sh
+++
@@ -1,3 +0,0 @@
-# Copy RxHttpClient.framework, because it's used as submodule and may be changed while development
-cp -R ./XCodeBuild/Products/Debug-iphonesimulator/RxHttpClient.framework ./Dependencies/iOS
-exit 0
