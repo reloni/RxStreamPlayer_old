e5721f9e8fe5a3494d7947b397079defa465020d
Anton Efimenko <reloni@ya.ru>
2016-07-12 15:20:25 +0400

    Make some fields and methods public to allow using outside  project (#7)
    * Setup 1
    
    * Delete submodules
    
    * Add workspace and rebuild dependencies
    
    * Rebuild dependent framework to allow testing
    
    * Disable embedding carthage frameworks
    
    * Rebuild dependencies
    
    * Make some fields and functions as public
    
    * Fix CopyInDevelopmentFrameworks.sh script to work only in Debug mode

--- Scripts/CopyInDevelopmentFrameworks.sh
+++ Scripts/CopyInDevelopmentFrameworks.sh
@@ -1,3 +1,6 @@
 # Copy frameworks that may be changed while development into dependencies folder, to allow Travis-CI use actual version for testing
-cp -R $1/Debug-iphonesimulator/RxHttpClient.framework ./Dependencies/iOS
+if [ "${CONFIGURATION}" = "Debug" ]; then
+	mkdir -p ./Dependencies/iOS
+	cp -R $1/Debug-iphonesimulator/RxHttpClient.framework ./Dependencies/iOS
+fi
 exit 0
